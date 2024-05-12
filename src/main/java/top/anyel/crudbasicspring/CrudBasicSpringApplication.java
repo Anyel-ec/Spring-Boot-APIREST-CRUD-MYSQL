6575f14dc813e2b856790219cac104a58a803fab
anyel-ec <paulangel826@gmail.com>
2024-05-12 08:56:54 -0500

    start project in spring 3.2.5, SDK 17

---
+++ src/main/java/top/anyel/crudbasicspring/CrudBasicSpringApplication.java
@@ -0,0 +1,13 @@
+package top.anyel.crudbasicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBasicSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudBasicSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/anyel/crudbasicspring/CrudBasicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package top.anyel.crudbasicspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBasicSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
